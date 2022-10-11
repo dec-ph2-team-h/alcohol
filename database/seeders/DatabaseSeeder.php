6b4a6d74f987e3b90e200e5ea8f31ae17d7b508b
harachaan <hr1021ry@icloud.com>
2022-10-12 02:52:44 +0900

    modify somethings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        // $this->call([AlcoholsSeeder::class]);
+        // \App\Models\User::factory(10)->create();
+        // 1行追加
+        $this->call([AlcoholsSeeder::class]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
