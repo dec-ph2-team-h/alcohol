b01796c268c4f1ca84e731446e6d5ae1eb837525
harachaan <hr1021ry@icloud.com>
2022-10-12 02:24:25 +0900

    add the function of seeder and the colums(alcohol_id, cups) in users_table

---
+++ database/seeders/AlcoholsSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+// 2行追加
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+// 1行追加
+use App\Models\Alcohol;
+
+class AlcoholsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 1行追加
+        Alcohol::factory()->count(10)->create();
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
+        // $this->call([AlcoholsSeeder::class]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
